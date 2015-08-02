b1fb45b34a039d378c6deb682338809f6f6b721b
Oliver Martell <oliver.martell@gmail.com>
2015-08-02 14:24:30 +0100

    Add support for parsing new-image, show-image and exit commands

--- src/pixels_cli/core.clj
+++ src/pixels_cli/core.clj
@@ -1,4 +1,22 @@
-(ns pixels-cli.core)
+(ns pixels-cli.core
+  (:require [clojure.string :as string]))
+
+(defn parse-command [str]
+  (let [chars (string/split str #" ")]
+    (case (first chars)
+      "X" {:command :exit}
+      "S" {:command :show-image}
+      "I" {:command :new-image :input {:M (Integer/parseInt (second chars))
+                                       :N (Integer/parseInt (last chars))}}
+      nil)))
+
+(defn terminate-session []
+  (println "Terminating session. Bye"))
 
 (defn -main [& args]
-  (println "Tiny Interactive Graphical Editor"))
+  (println "Tiny Interactive Graphical Editor")
+  (println "Enter the commands, one command per line:")
+  (loop [input (read-line)]
+    (if (= :exit (:command (parse-command input)))
+      (terminate-session)
+      (recur (read-line)))))
--- test/pixels_cli/core_test.clj
+++ test/pixels_cli/core_test.clj
@@ -1,3 +1,13 @@
 (ns pixels-cli.core-test
   (:require [clojure.test :refer :all]
             [pixels-cli.core :refer :all]))
+
+(deftest parse-commands
+  (testing "exit command"
+    (is (= {:command :exit} (parse-command "X"))))
+  (testing "new image command"
+    (is (= {:command :new-image
+            :input {:M 5 :N 4}}
+           (parse-command "I 5 4"))))
+  (testing "show current image"
+    (is (= {:command :show-image} (parse-command "S")))))
