dad75697e5cf27462e14a1d690424ed72895f3d3
Oliver Martell <oliver.martell@gmail.com>
2015-08-05 08:29:01 +0100

    Start rendering basic images

--- src/pixels_cli/core.clj
+++ src/pixels_cli/core.clj
@@ -6,17 +6,49 @@
     (case (first chars)
       "X" {:command :exit}
       "S" {:command :show-image}
-      "I" {:command :new-image :input {:M (Integer/parseInt (second chars))
-                                       :N (Integer/parseInt (last chars))}}
+      "I" {:command :new-image :input {:m (Integer/parseInt (second chars))
+                                       :n (Integer/parseInt (last chars))}}
       nil)))
 
 (defn terminate-session []
   (println "Terminating session. Bye"))
 
+(defn new-image [command _]
+  {:m (get-in command [:input :m])
+   :n (get-in command [:input :n])})
+
+(defn render-image [image]
+  (let [m (:m image 0)
+        n (:n image 0)
+        coloured (:coloured image)]
+    (dotimes [y n]
+      (dotimes [x m]
+        (print "O"))
+      (println ""))))
+
+(defn build-image [commands]
+  (println "commands: " commands)
+  (reduce (fn [image command]
+            (case (:command command)
+              :new-image (new-image command image)))
+          {}
+          commands))
+
+(defn process-command [command app-state]
+  (let [instruction (:command command)
+        history (:history app-state)]
+    (case instruction
+      :new-image  (assoc app-state :history (conj history command))
+      :show-image (render-image (build-image history))
+      app-state)))
+
 (defn -main [& args]
   (println "Tiny Interactive Graphical Editor")
   (println "Enter the commands, one command per line:")
-  (loop [input (read-line)]
-    (if (= :exit (:command (parse-command input)))
-      (terminate-session)
-      (recur (read-line)))))
+  (loop [command (parse-command (read-line))
+         app-state {:history []}]
+    (println "parsed: " command)
+    (if-not (= :exit (:command command))
+      (let [new-app-state (process-command command app-state)]
+        (recur (parse-command (read-line)) new-app-state)))))
+
--- test/pixels_cli/core_test.clj
+++ test/pixels_cli/core_test.clj
@@ -1,13 +1,40 @@
 (ns pixels-cli.core-test
   (:require [clojure.test :refer :all]
+            [clojure.string :as string]
             [pixels-cli.core :refer :all]))
 
-(deftest parse-commands
+(defn image [& strings]
+  (string/join (map #(str %1 "\n") strings)))
+
+(deftest processing-commands
+  (testing "new image"
+    (is (= {:history [{:command :new-image}]}
+           (process-command {:command :new-image}
+                            {:history []}))))
+  (testing "not recognised command"
+    (is (= {:history []}
+           (process-command {:command :launch-rocket}
+                            {:history []})))))
+
+(deftest building-images
+  (is (= {:m 4 :n 3}
+         (build-image [{:command :new-image
+                        :input {:m 4 :n 3}}]))))
+
+(deftest rendering-images
+  (is (= (image "OOOO"
+                "OOOO"
+                "OOOO")
+         (with-out-str (render-image {:m 4
+                                      :n 3
+                                      :coloured {}})))))
+
+(deftest parsing-commands
   (testing "exit command"
     (is (= {:command :exit} (parse-command "X"))))
   (testing "new image command"
     (is (= {:command :new-image
-            :input {:M 5 :N 4}}
+            :input {:m 5 :n 4}}
            (parse-command "I 5 4"))))
   (testing "show current image"
     (is (= {:command :show-image} (parse-command "S")))))
